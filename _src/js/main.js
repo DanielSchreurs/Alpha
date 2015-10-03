3959b05c7e1c72f875d47ab4c0b91289c5f91651
Daniel <daniel.schreurs@hotmail.com>
2015-10-03 15:51:14 +0200

    for the search tool

---
+++ _src/js/main.js
@@ -0,0 +1,10 @@
+(
+    function () {
+        $(function () {
+            $(".show-search").click(function () {
+                    $('.search-form').toggleClass("search-form--open");
+                    console.log('oui');
+            });
+        });
+    }
+)();
