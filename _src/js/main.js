20ded6e612ce7553bd6aeccd7d0a89b508d6d5fd
Daniel <daniel.schreurs@hotmail.com>
2015-10-04 16:13:54 +0200

    add navigation

--- _src/js/main.js
+++ _src/js/main.js
@@ -2,8 +2,17 @@
     function () {
         $(function () {
             $(".show-search").click(function () {
-                    $('.search-form').toggleClass("search-form--open");
-                    console.log('oui');
+                $('.search-form').toggleClass("search-form--open");
+            });
+
+            $('.carte-container').click(function () {
+                $('.carte-container iframe').css("pointer-events", "auto");
+            });
+
+            $('.open_nav').on("click",function(){
+                event.preventDefault();
+                $(this).toggleClass("active");
+                $('.menu-container').toggleClass("menu-container--open");
             });
         });
     }
