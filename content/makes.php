d98ad97582979d09627a94cdca5cb840510c8df3
Daniel <daniel.schreurs@hotmail.com>
2015-10-06 23:49:38 +0200

    show what they do

--- content/makes.php
+++ content/makes.php
@@ -1,73 +1,69 @@
-<div class="color-container color-container--white section section--first">
-    <div class="wrapper">
-        <ul class="grid-layout">
-            <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
-                <svg viewBox="0 0 100 100" class="icon antiman shape-hand">
-                    <use xlink:href="#shape-hand"></use>
-                </svg>
-                <div class="media-vertical__body">
-                    <h2 class="gamma make_item__header">LE PREMIER ACCUEIL</h2>
+<?php $query = new WP_Query(array('post_type' => 'Accueil')); ?>
+<?php while ($query->have_posts()) : $query->the_post(); ?>
+    <div class="color-container color-container--white section section--first">
+        <div class="wrapper">
+            <ul class="grid-layout">
+                <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
+                    <svg viewBox="0 0 100 100" class="icon antiman shape-hand">
+                        <use xlink:href="#shape-hand"></use>
+                    </svg>
+                    <div class="media-vertical__body">
+                        <h2 class="gamma make_item__header"><?php the_field('titre_1'); ?></h2>
 
-                    <p>Nous travaillons sur rendez-vous. Les situations d'urgence peuvent être prises en charge, si
-                        nécessaire, par un travailleur de rue.</p>
-                </div>
-            </li>
-            <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
-                <svg viewBox="0 0 100 100" class="icon antiman shape-social">
-                    <use xlink:href="#shape-social"></use>
-                </svg>
-                <div class="media-vertical__body">
-                    <h2 class="gamma make_item__header">PSYCHOSOCIAL</h2>
+                        <p><?php the_field('texte_1'); ?></p>
+                    </div>
+                </li>
+                <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
+                    <svg viewBox="0 0 100 100" class="icon antiman shape-social">
+                        <use xlink:href="#shape-social"></use>
+                    </svg>
+                    <div class="media-vertical__body">
+                        <h2 class="gamma make_item__header"><?php the_field('titre_2'); ?></h2>
 
-                    <p>Les travailleurs sociaux peuvent rencontrer régulièrement les personnes pour les aider à
-                        résoudre les difficultés d'ordre social (problèmes de justice, financiers, administratifs,
-                        ...)</p>
-                </div>
-            </li>
-            <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
-                <svg viewBox="0 0 100 100" class="icon antiman shape-psy">
-                    <use xlink:href="#shape-psy"></use>
-                </svg>
-                <div class="media-vertical__body">
-                    <h2 class="gamma make_item__header">PSYCHOTHERAPIES</h2>
+                        <p><?php the_field('texte_2'); ?></p>
+                    </div>
+                </li>
+                <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
+                    <svg viewBox="0 0 100 100" class="icon antiman shape-psy">
+                        <use xlink:href="#shape-psy"></use>
+                    </svg>
+                    <div class="media-vertical__body">
+                        <h2 class="gamma make_item__header"><?php the_field('titre_3'); ?></h2>
 
-                    <p>Nous proposons aux personnes qui le souhaitent d'aborder plus profondément les difficultés
-                        psychologiques liées ou non à une problématique de dépendance.</p>
-                </div>
-            </li>
-            <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
-                <svg viewBox="0 0 100 100" class="icon antiman shape-medicale">
-                    <use xlink:href="#shape-medicale"></use>
-                </svg>
-                <div class="media-vertical__body">
-                    <h2 class="gamma make_item__header">CONSULTATIONS MÉDICALES</h2>
+                        <p><?php the_field('texte_3'); ?></p>
+                    </div>
+                </li>
+                <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
+                    <svg viewBox="0 0 100 100" class="icon antiman shape-medicale">
+                        <use xlink:href="#shape-medicale"></use>
+                    </svg>
+                    <div class="media-vertical__body">
+                        <h2 class="gamma make_item__header"><?php the_field('titre_4'); ?></h2>
 
-                    <p>Nous proposons aux personnes qui le souhaitent d'aborder plus profondément les difficultés
-                        psychologiques liées ou non à une problématique de dépendance.</p>
-                </div>
-            </li>
-            <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
-                <svg viewBox="0 0 100 100" class="icon antiman shape-group">
-                    <use xlink:href="#shape-group"></use>
-                </svg>
-                <div class="media-vertical__body">
-                    <h2 class="gamma make_item__header">GROUPES D'ENTRAIDE</h2>
+                        <p><?php the_field('texte_4'); ?></p>
+                    </div>
+                </li>
+                <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
+                    <svg viewBox="0 0 100 100" class="icon antiman shape-group">
+                        <use xlink:href="#shape-group"></use>
+                    </svg>
+                    <div class="media-vertical__body">
+                        <h2 class="gamma make_item__header"><?php the_field('titre_5'); ?></h2>
 
-                    <p>Les groupes sont des lieux d'échange, de reconnaissance de soi et de l'autre, de soutien
-                        réciproque, de confrontations d'expériences.</p>
-                </div>
-            </li>
-            <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
-                <svg viewBox="0 0 100 100" class="icon antiman shape-therapie">
-                    <use xlink:href="#shape-therapie"></use>
-                </svg>
-                <div class="media-vertical__body">
-                    <h2 class="gamma make_item__header">SERVICE PARENTALITE</h2>
+                        <p><?php the_field('texte_5'); ?></p>
+                    </div>
+                </li>
+                <li class="make_item grid-layout__item u-1/3 u-1/2-lap u-1/1-palm media-vertical">
+                    <svg viewBox="0 0 100 100" class="icon antiman shape-therapie">
+                        <use xlink:href="#shape-therapie"></use>
+                    </svg>
+                    <div class="media-vertical__body">
+                        <h2 class="gamma make_item__header"><?php the_field('titre_6'); ?></h2>
 
-                    <p>Ce Service a été mis en place en 1994. Il est assuré par une équipe psycho- médico-sociale
-                        spécifique. L'objectif de ce service est double</p>
-                </div>
-            </li>
-        </ul>
+                        <p><?php the_field('texte_6'); ?></p>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
-</div>
+<?php endwhile; ?>
