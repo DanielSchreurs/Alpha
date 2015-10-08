b8f2cdcf5f25a562563841edd41032e6461fb845
Daniel <daniel.schreurs@hotmail.com>
2015-10-08 08:28:06 +0200

    init page 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<!--[if IE 8]
+    <p class="section__paragraphe">Votre Navigateur n’est pas à jour. Mettez-le à jour<a href="http://browsehappy.com/">ici.</a></p><![endif]-->
+<main role="main">
+    <div class="text--center wrapper"><h1 class="header--404"><b>404</b>
+            <span>Oups, la page demandée ne semble pas exister</span></h1>
+
+        <p class="visuallyhidden"> La page que vous venez de demandez, n'existe pas. Je vous invite à vérifier l'url de
+            la page dans votre navigateur.</p>
+
+        <div class="both-btn"><a href="http://danielschreurs.com" title="Renvoie vers la page d'accueil"
+                                 class="btn btn--huge beta simple-gray transition btn--soft">Retourner à l’accueil</a>
+        </div>
+</main>
+<?php get_footer(); ?>
