45a632282a6ae38c30b0374bdd455ff3133ab9fe
Daniel <daniel.schreurs@hotmail.com>
2015-10-08 09:29:42 +0200

    init content

--- content/contact.php
+++ content/contact.php
@@ -3,6 +3,6 @@
 <div class="wrapper contact_container section">
     <p class="container__header"><?php the_field('titre_de_contact'); ?></p>
 
-    <p class="text--center contact_meta zeta"> <?php the_field('slogan_de_contact'); ?></p><a href="#" class="btn btn--huge beta simple-gray transition btn--soft">Nous contacter</a>
+    <p class="text--center contact_meta zeta"><?php the_field('slogan_de_contact'); ?></p><a href="#" class="btn btn--huge beta simple-gray transition btn--soft">Nous contacter</a>
 </div>
 <?php endwhile; ?>
