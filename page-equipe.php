9eb65f9c286d10eb4c6cc0f473ef593eebc37dec
Daniel <daniel.schreurs@hotmail.com>
2015-10-08 10:33:27 +0200

    init page

---
+++ page-equipe.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<!--[if IE 8]
+    <p class="section__paragraphe">Votre Navigateur n’est pas à jour. Mettez-le à jour<a href="http://browsehappy.com/">ici.</a></p><![endif]-->
+<main role="main" class="wrapper main article-container ">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1 class="big-header"><?php the_title(); ?></h1>
+        <div class="article">
+            <p class="article_paragraph"><?php the_content(); ?></p>
+        </div>
+    <?php endwhile;
+    else: ?>
+        <h1 class="article_header"><?php _e('Sorry, no posts matched your criteria.'); ?></h1>
+    <?php endif; ?>
+
+    <div>
+        bouh
+    </div>
+</main>
+<?php get_footer(); ?>
