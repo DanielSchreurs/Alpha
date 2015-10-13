5d6beb8bf085219e68abac8ba8b17ea8f08ead57
Daniel <daniel.schreurs@hotmail.com>
2015-10-13 23:05:32 +0200

    add own taxonomie

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
 function theme_name_scripts()
 {
     wp_enqueue_style('daniel', get_stylesheet_directory_uri() . '/css/main.css');
-    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js',array( 'jquery' ),'1.0.0',true);
-    wp_enqueue_script('slider', get_template_directory_uri() . '/js/slider.js',array( 'jquery' ),'1.0.0',true);
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('slider', get_template_directory_uri() . '/js/slider.js', array('jquery'), '1.0.0', true);
 }
 
 
@@ -15,69 +15,77 @@ function gkp_html_minify_start()
 {
     ob_start('gkp_html_minyfy_finish');
 }
-function register_my_menus() {
+
+function register_my_menus()
+{
     register_nav_menus(
         array(
-            'header-menu' => __( 'Main ' )
+            'header-menu' => __('Main '),
+            'liens_utiles' => __('Liens utiles')
         )
     );
 }
-function new_nav_menu_items($items) {
-    $homelink = '<li class="main-vavigation__link main-vavigation__link--home"><a href="' . home_url( '/' ) . '">' . __('Home') . '</a></li>';
+
+function new_nav_menu_items($items)
+{
+    $homelink = '<li class="main-vavigation__link main-vavigation__link--home"><a href="' . home_url('/') . '">' . __('Home') . '</a></li>';
     $items = $homelink . $items;
     return $items;
 }
 
-function create_post_type() {
-    register_post_type( 'Accueil',
+function create_post_type()
+{
+    register_post_type('Accueil',
         array(
             'labels' => array(
-                'name' => __( 'Accueil' ),
-                'singular_name' => __( 'Accueil' )
+                'name' => __('Accueil'),
+                'singular_name' => __('Accueil')
             ),
             'public' => true,
             'has_archive' => true,
-            'supports'=>false
+            'supports' => false
         )
     );
-    register_post_type( 'slider',
+    register_post_type('slider',
         array(
             'labels' => array(
-                'name' => __( 'slider' ),
-                'singular_name' => __( 'slider' )
+                'name' => __('slider'),
+                'singular_name' => __('slider')
             ),
             'public' => true,
             'has_archive' => true,
-            'supports'=>['title']
+            'supports' => ['title']
         )
     );
-    register_post_type( 'equipe',
+    register_post_type('equipe',
         array(
             'labels' => array(
-                'name' => __( 'Équipe' ),
-                'singular_name' => __( 'Équipe' )
+                'name' => __('Membres'),
+                'singular_name' => __('Membre')
             ),
             'public' => true,
             'has_archive' => true,
-            'supports'=>['title']
+            'supports' => ['title']
         )
     );
-    register_post_type( 'contact',
+    register_post_type('contact',
         array(
             'labels' => array(
-                'name' => __( 'contact' ),
-                'singular_name' => __( 'contact' )
+                'name' => __('Personnes de contact'),
+                'singular_name' => __('contact')
             ),
             'public' => true,
             'has_archive' => true,
-            'supports'=>['title']
+            'supports' => ['title']
         )
     );
 }
 
-function my_function_admin_bar(){
+function my_function_admin_bar()
+{
     return false;
 }
+
 function gkp_html_minyfy_finish($html)
 {
 
@@ -94,9 +102,48 @@ function gkp_html_minyfy_finish($html)
 
     return $html;
 }
-add_action( 'init', 'create_post_type' );
-add_filter( 'show_admin_bar' , 'my_function_admin_bar');
+
+function people_init()
+{
+    register_taxonomy(
+        'Formations',
+        'equipe',
+        array(
+            'label' => __('Formations'),
+            'show_ui' => true,
+            'show_in_quick_edit' => true,
+            'hierarchical' => true,
+            'show_in_menu'=>false
+        )
+    );
+    register_taxonomy(
+        'Fonctions',
+        'equipe',
+        array(
+            'label' => __('Fonctions'),
+            'show_ui' => true,
+            'show_in_quick_edit' => true,
+            'hierarchical' => true,
+            'show_in_menu'=>false
+        )
+    );
+    register_taxonomy(
+        'team',
+        'equipe',
+        array(
+            'label' => __('Équipe'),
+            'show_ui' => true,
+            'show_in_quick_edit' => true,
+            'hierarchical' => true,
+            'show_in_menu'=>false
+        )
+    );
+}
+
+add_action('init', 'people_init');
+add_action('init', 'create_post_type');
 add_action('wp_enqueue_scripts', 'theme_name_scripts');
 add_action('get_header', 'gkp_html_minify_start');
-add_action( 'init', 'register_my_menus' );
-add_filter( 'wp_nav_menu_items', 'new_nav_menu_items' );
+add_action('init', 'register_my_menus');
+add_filter('show_admin_bar', 'my_function_admin_bar');
+add_filter('wp_nav_menu_items', 'new_nav_menu_items');
