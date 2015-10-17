b48c73cfed781e4ed55c940337d53fd28c5ed737
Daniel <daniel.schreurs@hotmail.com>
2015-10-17 23:44:43 +0200

    add wrapper

--- content/contact.php
+++ content/contact.php
@@ -1,6 +1,6 @@
 <?php $query = new WP_Query(array('post_type' => 'Accueil')); ?>
 <?php while ($query->have_posts()) : $query->the_post(); ?>
-<div class="wrapper contact_container section">
+    <div class="wrapper contact_container section">
     <p class="container__header"><?php the_field('titre_de_contact'); ?></p>
     <p class="text--center contact_meta zeta"><?php the_field('slogan_de_contact'); ?></p><a href="<?php echo(get_page_link(10)); ?>" class="btn btn--huge beta simple-gray transition btn--soft">Nous contacter</a>
 </div>
