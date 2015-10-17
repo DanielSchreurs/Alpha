6bf1a5541f6d4693c3f6115336fe9c201792c7d6
Daniel <daniel.schreurs@hotmail.com>
2015-10-17 23:45:02 +0200

    fixe min.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-(
-    function () {
+(function () {
 
-        jQuery(function () {
-            jQuery(".show-search").click(function () {
-                jQuery('.search-form').toggleClass("search-form--open");
-            });
-            jQuery('.open_nav').on("click",function(){
-                event.preventDefault();
-                jQuery(this).toggleClass("active");
-                jQuery('.menu-container').toggleClass("menu-container--open");
-            });
-        });
-    }
-)();
+		jQuery( function () {
+			jQuery( ".show-search" ).click( function () {
+				jQuery( '.search-form' ).toggleClass( "search-form--open" );
+			} );
+			jQuery( '.open_nav' ).on( "click", function () {
+				event.preventDefault();
+				jQuery( this ).toggleClass( "active" );
+				jQuery( '.menu-container' ).toggleClass( "menu-container--open" );
+			} );
+			jQuery( '.sub-menu a' ).on('focus',function(){
+				jQuery('.sub-menu').addClass('sub-menu--active');
+			});
+			jQuery( '.menu-item-has-children+li a' ).on('focus',function(){
+				jQuery('.sub-menu').removeClass('sub-menu--active');
+			});
+		} );
+	})();
