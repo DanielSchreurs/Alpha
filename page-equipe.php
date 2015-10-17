fec2fcd266360cbd3450eb110bb20e996b137b99
Daniel <daniel.schreurs@hotmail.com>
2015-10-17 23:45:11 +0200

    fixe frapper

--- page-equipe.php
+++ page-equipe.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <!--[if IE 8]
     <p class="section__paragraphe">Votre Navigateur n’est pas à jour. Mettez-le à jour<a href="http://browsehappy.com/">ici.</a></p><![endif]-->
-<main role="main" class="main">
+<main role="main" class="main wrapper">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1 class="big-header wrapper"><?php the_title(); ?></h1>
-        <div class="article wrapper article_paragraph">
+        <h1 class="big-header"><?php the_title(); ?></h1>
+        <div class="article article_paragraph">
            <?php the_content(); ?>
         </div>
     <?php endwhile;
@@ -12,7 +12,7 @@
         <h1 class="article_header"><?php _e('Sorry, no posts matched your criteria.'); ?></h1>
     <?php endif; ?>
     <?php $query = new WP_Query(array('orderby' => 'title', 'order' => 'ASC', 'post_type' => 'equipe')); ?>
-    <ul class="grid-layout wrapper">
+    <ul class="grid-layout">
         <?php while ($query->have_posts()) : $query->the_post(); ?>
             <?php $image = get_field('image_personnel') ?>
             <li class="people-container grid-layout__item u-1/3 u-1/2-lap u-1/1-palm">
