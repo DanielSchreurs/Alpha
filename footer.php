c4a8168466eeb7399ee9e6551a81cbbbc7dbb110
Daniel <daniel.schreurs@hotmail.com>
2015-10-18 14:43:23 +0200

    add information

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <footer class="wrapper">
         <div class="grid-layout">
             <div class="grid-layout__item u-1/4 u-1/2-lap u-1/1-palm">
-                <p class="beta footer_header">Informations</p>
+                <p class="beta footer_header"><?php _e('Navigation'); ?></p>
                 <ul class="list-bare footer__navigation">
                     <li><a href="#" class="delta footer__link transition link">Foire aux questions</a></li>
                     <li><a href="#" class="delta footer__link transition link">Liens utiles</a></li>
@@ -11,13 +11,19 @@
                 </ul>
             </div>
             <div class="grid-layout__item u-1/4 u-1/2-lap u-1/1-palm vcard">
-                <p class="beta footer_header">Adresse</p>
-                <ul class="list-bare footer__navigation">
-                    <li><a href="#" class="delta footer__link transition link">Rue des Madelaine</a></li>
-                    <li><a href="#" class="delta footer__link transition link">17 4000 Liège</a></li>
-                    <li><a href="#" class="delta footer__link transition link tel">Fax : 04/223.56.86</a></li>
-                    <li><a href="#" class="delta footer__link transition link tel">Fax : 04/223.56.86</a></li>
-                </ul>
+             <?php $query = new WP_Query(array('post_type' => 'page')); ?>
+                <?php while ($query->have_posts()) : $query->the_post(); ?>
+                <?php if (get_the_ID() === 10): ?>
+                    <p class="beta footer_header"><?php _e('Informations'); ?></p>
+                    <ul class="list-bare footer__navigation">
+                        <li><?php the_field('telephone'); ?></li>
+                        <li><?php the_field('fax'); ?></li>
+                        <li><?php the_field('n_dentreprise'); ?></li>
+                        <li><?php the_field('compte'); ?></li>
+                        <li><?php the_field('horaires'); ?></li>
+                    </ul>
+                <?php endif; ?>
+                <?php endwhile; ?>
             </div>
             <div class="grid-layout__item u-1/4 u-1/2-lap u-1/1-palm vcard">
                 <p class="beta footer_header">Nous suivre</p>
