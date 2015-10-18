10a9fe8e5155fc669f3ba6d0bbdf338ffd289275
Daniel <daniel.schreurs@hotmail.com>
2015-10-18 14:24:50 +0200

    add style to the slider

--- content/slider.php
+++ content/slider.php
@@ -1,7 +1,7 @@
 <?php $query = new WP_Query(array('post_type' => 'slider')); ?>
 <?php if ($query->have_posts()): ?>
     <div class="slider-container">
-        <div class="wrapper slider-wrapper">
+        <div class="wrapper slider-wrapper text--center">
             <div id="slider" class="slider">
                 <ul>
                     <?php while ($query->have_posts()) : $query->the_post(); ?>
@@ -18,6 +18,7 @@
                     <use xlink:href="#shape-next"></use>
                 </svg>
             </a>
+            <a class="btn btn-round btn-round--bold transition--soft btn-round--slider" href="<?php echo(get_page_link('10')); ?>">Nous contacter</a>
         </div>
     </div>
 <?php endif; ?>
