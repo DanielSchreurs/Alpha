d7f3fb08a14e47809727223cfab129d3626e83d5
Daniel <daniel.schreurs@hotmail.com>
2015-10-21 16:50:54 +0200

    try to make a search page

--- search.php
+++ search.php
@@ -14,44 +14,35 @@
  * @since Twenty Fourteen 1.0
  */
 
-get_header();?>
+get_header(); ?>
 <!--[if IE 8]
     <p class="section__paragraphe">Votre Navigateur n’est pas à jour. Mettez-le à jour<a href="http://browsehappy.com/">ici.</a></p><![endif]-->
-<main role="main">
+<main role="main" class="main wrapper">
 
-    <?php if ( have_posts() ) : ?>
-
-        <header class="page-header">
-            <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyfifteen' ), get_search_query() ); ?></h1>
-        </header><!-- .page-header -->
-
+    <?php if (have_posts()) : ?>
+        <h1 class="big-header"><?php echo(__('Résultats pour : ') . get_search_query()); ?></h1>
         <?php
         // Start the loop.
-        while ( have_posts() ) : the_post(); ?>
+        $_SESSION['i'] = 0;
+        while (have_posts()) : the_post(); ?>
+            <?php $_SESSION['i'] = $_SESSION['i']+1;?>
+            <?php get_template_part('content','search'); ?>
 
-            <?php
-            /*
-             * Run the loop for the search to output the results.
-             * If you want to overload this in a child theme then include a file
-             * called content-search.php and that will be used instead.
-             */
-            get_template_part( 'content', 'search' );
-
-            // End the loop.
-        endwhile;
-
-        // Previous/next page navigation.
-        the_posts_pagination( array(
-            'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-            'next_text'          => __( 'Next page', 'twentyfifteen' ),
-            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-        ) );
-
-    // If no content, include the "No posts found" template.
-    else :
-        get_template_part( 'content', 'none' );
-
-    endif;
-    ?>
+        <?php endwhile;?>
+        <?php // Previous/next page navigation.
+        the_posts_pagination(array(
+            'prev_text' => __('Précédent'),
+            'next_text' => __('Suivant'),
+            'before_page_number' => '',
+            'after_page_number' => ' >',
+            'screen_reader_text'=>'La navigation entre les articles'
+        )); ?>
+    <?php else : ?>
+        <h1 class="header--404">
+        <b><?php _e('Oups'); ?>&hellip;</b>
+        <span><?php echo(__('Nous n’avons pas trouvé de résulats pour :') . get_search_query()); ?></span>
+    </h1>
+        <div class="search-form-container search-form-container--no-result"><?php get_search_form(true); ?></div>
+    <?php endif; ?>
 </main>
 <?php get_footer(); ?>
