e972d7ed416782b253a6242a06e78048888b3bb0
Daniel <daniel.schreurs@hotmail.com>
2015-10-21 08:03:56 +0200

    add contact

--- content/contact.php
+++ content/contact.php
@@ -2,6 +2,7 @@
 <?php while ($query->have_posts()) : $query->the_post(); ?>
     <div class="wrapper contact_container section">
     <p class="container__header"><?php the_field('titre_de_contact'); ?></p>
-    <p class="text--center contact_meta zeta"><?php the_field('slogan_de_contact'); ?></p><a href="<?php echo(get_page_link(10)); ?>" class="btn btn--huge beta simple-gray transition btn--soft">Nous contacter</a>
+    <p class="text--center contact_meta zeta"><?php the_field('slogan_de_contact'); ?></p>
+    <span class="btn btn--huge beta simple-gray transition btn--soft"><?php the_field('numero_de_telephone'); ?></span>
 </div>
 <?php endwhile; ?>
