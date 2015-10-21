8d0de01d38cd9cd6f03d3878255fbbea1c044fba
Daniel <daniel.schreurs@hotmail.com>
2015-10-21 08:02:16 +0200

    init content

---
+++ content-contact.php
@@ -0,0 +1,6 @@
+<li class="grid-layout__item u-1/3 u-1/2-lap u-1/1-palm">
+    <div class="grey-blog">
+        <span class="grey-blog__item grey-blog__item--light"><?php the_field('nom_de_contact'); ?></span>
+        <span class="grey-blog__item"><?php the_field('adresse'); ?></span>
+    </div>
+</li>
