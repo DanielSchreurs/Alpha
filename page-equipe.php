c975829f73406c739a4499cfdfc32372e7b5f868
Daniel <daniel.schreurs@hotmail.com>
2015-10-21 16:53:50 +0200

    add a page equipe

---
+++ page-equipe.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<!--[if IE 8]
+    <p class="section__paragraphe">Votre Navigateur n’est pas à jour. Mettez-le à jour<a href="http://browsehappy.com/">ici.</a></p><![endif]-->
+<main role="main" class="main wrapper">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1 class="big-header"><?php the_title(); ?></h1>
+        <div class="article article_paragraph">
+           <?php the_content(); ?>
+        </div>
+    <?php endwhile;
+    else: ?>
+        <h1 class="article_header"><?php _e('Sorry, no posts matched your criteria.'); ?></h1>
+    <?php endif; ?>
+    <?php $query = new WP_Query(array('orderby' => 'title', 'order' => 'ASC', 'post_type' => 'equipe')); ?>
+    <ul class="grid-layout">
+        <?php while ($query->have_posts()) : $query->the_post(); ?>
+            <?php get_template_part('content', 'equipe'); ?>
+        <?php endwhile; ?>
+    </ul>
+</main>
+<?php get_footer(); ?>
