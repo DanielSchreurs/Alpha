b2c09bb91d32fabde8914fc25804ed75391dacdc
Daniel <daniel.schreurs@hotmail.com>
2015-10-24 15:28:55 +0200

    add institution box

---
+++ content-institutions.php
@@ -0,0 +1,8 @@
+<li class="grid-layout__item u-1/3 u-1/2-lap u-1/1-palm">
+    <div class="prince-container prince-container--institution">
+        <p class="institution__tel__title"><?php the_field('le_nom_de_linstitution'); ?></p>
+        <p class="institution__tel__open"><?php the_field('heure_douverture'); ?></p>
+        <p class="gamma institution__tel"><span class=""><?php the_field('le_nom_de_la_personne_de_contact_1'); ?>&nbsp;</span><?php the_field('numero_de_la_personne_de_contact_1'); ?></p>
+        <p class="gamma institution__tel"><span class=""><?php the_field('le_nom_de_la_personne_de_contact_2'); ?>&nbsp;</span><?php the_field('numero_de_la_personne_de_contact_2'); ?></p>
+    </div>
+</li>
