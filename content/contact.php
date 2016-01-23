771ec4816bbff4eb96aad6611691a0e81e7a6e2f
Daniel <daniel.schreurs@hotmail.com>
2016-01-23 20:41:45 +0100

    update contact

--- content/contact.php
+++ content/contact.php
@@ -1,8 +1,11 @@
-<?php $query = new WP_Query(array('post_type' => 'Accueil')); ?>
+<?php $query = new WP_Query(['post_type' => 'Accueil']); ?>
 <?php while ($query->have_posts()) : $query->the_post(); ?>
-    <div class="wrapper contact_container section">
-    <p class="container__header"><?php the_field('titre_de_contact'); ?></p>
-    <p class="text--center contact_meta zeta"><?php the_field('slogan_de_contact'); ?></p>
-    <span class="btn btn--huge beta simple-gray transition btn--soft"><?php the_field('numero_de_telephone'); ?></span>
-</div>
+    <div class="color-container--orange color-container--orange--contact" >
+        <div class="wrapper contact_container section">
+            <p class="container__header"><?php the_field('titre_de_contact'); ?></p>
+            <p class="text--center contact_meta zeta"><?php the_field('slogan_de_contact'); ?></p>
+            <span
+                class="btn btn--huge beta not-btné simple-white transition btn--soft"><?php the_field('numero_de_telephone'); ?></span>
+        </div>
+    </div>
 <?php endwhile; ?>
